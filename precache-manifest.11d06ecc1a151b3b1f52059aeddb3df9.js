7db956aa1686147af1a525e60caa44c011bc607a
MirEnovi <garciaivonne11@gmail.com>
2019-08-10 02:18:33 -0500

    Updates

---
+++ precache-manifest.11d06ecc1a151b3b1f52059aeddb3df9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480afd3188b4c6a39bde56fdceaa14b3",
+    "url": "/Digital_Coster_Test/index.html"
+  },
+  {
+    "revision": "7a8a9257e0918ad7b6f6",
+    "url": "/Digital_Coster_Test/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/Digital_Coster_Test/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "7a8a9257e0918ad7b6f6",
+    "url": "/Digital_Coster_Test/static/js/main.ac13f6e0.chunk.js"
+  },
+  {
+    "revision": "adc2d6888f17307765ec",
+    "url": "/Digital_Coster_Test/static/js/runtime~main.b5024c53.js"
+  }
+]);
